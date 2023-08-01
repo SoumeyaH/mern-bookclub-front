02f123fca7716d9a074e37db45fc12915a19e7b0
soumeyaH <soumeyahassan47@gmail.com>
2023-08-01 19:52:04 +0100

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import PageNotFound from "./pages/PageNotFound";
+
 import './App.css'
 
 function App() {
- 
   return (
-    <div>hello</div>
-  )
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
